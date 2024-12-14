433e66fe76262d23f66d30b19b63eae59f35b4ff
timon-yego <kipyegotimothy99@gmail.com>
2024-12-15 02:22:01 +0300

    Modified test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -88,8 +88,7 @@ class TestGithubOrgClient(unittest.TestCase):
         Test the _public_repos_url method of GithubOrgClient.
         """
         # Define a sample payload
-        mock_get_json.return_value = {"repos_url":
-            "https://api.github.com/orgs/google/repos"}
+        mock_get_json.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
         mock_public_repos_url.return_value = (
             "https://api.github.com/orgs/google/repos"
         )
