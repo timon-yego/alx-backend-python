2558ff92d5dc9eebeb47a15ab633e61146c988f5
timon-yego <kipyegotimothy99@gmail.com>
2025-01-12 00:38:55 +0300

    Implement signals for user notifications

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -25,6 +25,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='edited_message')
 
     def __str__(self):
         return f"History of message {self.message.id} edited at {self.edited_at}"
